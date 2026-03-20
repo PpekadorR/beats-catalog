3af72cc518ff423031f8d1d1bb4feec1d3f243be
PpekadorR <168614205+PpekadorR@users.noreply.github.com>
2026-03-20 02:35:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,31 @@
-const wavesurfer = WaveSurfer.create({
-
-container:"#waveform",
-waveColor:"#444",
-progressColor:"#1db954",
-height:80,
-responsive:true
-
-})
+const player = new Plyr('#player')
 
 const tracks = document.querySelectorAll(".track")
-const playBtn = document.getElementById("playBtn")
 
 const title = document.getElementById("title")
 const meta = document.getElementById("meta")
-const time = document.getElementById("time")
-
-let loaded = false
 
 tracks.forEach(track=>{
 
 track.addEventListener("click",()=>{
 
-tracks.forEach(t=>t.classList.remove("active"))
-track.classList.add("active")
-
 const src = track.dataset.src
+
+player.source = {
+type: 'audio',
+sources: [
+{
+src: src,
+type: 'audio/mp3',
+},
+],
+}
 
 title.innerText = track.dataset.title
 meta.innerText = track.dataset.meta
 
-loaded = false
-
-wavesurfer.load(src)
+player.play()
 
 })
-
-})
-
-wavesurfer.on("ready",()=>{
-
-loaded = true
-wavesurfer.play()
-playBtn.innerText="⏸"
-
-})
-
-playBtn.addEventListener("click",()=>{
-
-if(!loaded) return
-
-if(wavesurfer.isPlaying()){
-
-wavesurfer.pause()
-playBtn.innerText="▶"
-
-}else{
-
-wavesurfer.play()
-playBtn.innerText="⏸"
-
-}
-
-})
-
-wavesurfer.on("audioprocess",()=>{
-
-const current = wavesurfer.getCurrentTime()
-
-let minutes = Math.floor(current/60)
-let seconds = Math.floor(current%60)
-
-if(seconds<10) seconds="0"+seconds
-
-time.innerText = minutes + ":" + seconds
 
 })
